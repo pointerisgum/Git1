37d81a51bfd76e6313c2411760d639d4fc765b24
Young-Min Kim <pointerisgum@me.com>
2016-01-13 12:43:01 +0900

    Initial Commit

---
+++ Git1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git1
+//
+//  Created by seon myeong su on 2016. 1. 13..
+//  Copyright © 2016년 Rowem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git1
+//
+//  Created by seon myeong su on 2016. 1. 13..
+//  Copyright © 2016년 Rowem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
